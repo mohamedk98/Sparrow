f9ad18a156815bbede7a5fca3054bea7263441a7
AbdelHameed-Sayed <94986615+AbdelHameed-Sayed@users.noreply.github.com>
2022-06-27 01:47:31 +0200

    Login component design and authentication is done

--- FE/src/components/login/LoginForm.js
+++ FE/src/components/login/LoginForm.js
@@ -1,11 +1,39 @@
 import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
 import { Link } from 'react-router-dom';
-import React from 'react';
+import React, { useState } from 'react';
 import facebook from '../../assets/icons/facebook.svg';
-import LoginInput from '../../pages/login/LoginInput';
+import eyeShow from '../../assets/icons/eye-password-show.svg';
+import eyeHide from '../../assets/icons/eye-password-hide.svg';
+import LoginInput from './LoginInput';
 import LoginButton from './LoginButton';
 
+const loginInfo = {
+  email: '',
+  password: '',
+};
+
 const LoginForm = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [login, setLogin] = useState(loginInfo);
+  const { email, password } = login;
+  // console.log(email, password);
+
+  const togglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const loginHandler = e => {
+    // console.log(e.target.name);
+    const { name, value } = e.target;
+    setLogin({ ...login, [name]: value });
+  };
+
+  const loginValidation = Yup.object({
+    email: Yup.string().required('Email address is required').email().max(100),
+    password: Yup.string().required('Password is required'),
+  });
+
   return (
     <div className="grid md:grid-cols-2 grid-cols-1">
       <div className="flex flex-col md:ml-10 px-36 md:px-0 mt-20">
@@ -22,21 +50,35 @@ const LoginForm = () => {
       </div>
       <div className="lg:ml-24 md:ml-20 mx-auto mt-10 w-3/4 text-center">
         <div className="block rounded-lg shadow-lg bg-white p-4">
-          <Formik className="p-6">
+          <Formik
+            enableReinitialize // To inforce it to teset form input values when initialValues changes.
+            initialValues={{ email, password }}
+            validationSchema={loginValidation}
+          >
             {formic => (
               <Form className="flex flex-col">
                 <LoginInput
                   name="email"
                   type="text"
                   placeholder="Email address"
-                  className="border-2 rounded-md p-3 mb-2 w-full outline-indigo-400"
+                  className="border-2 rounded-md p-3 mb-2 w-full"
+                  onChange={loginHandler}
                 />
-                <LoginInput
-                  type="text"
-                  placeholder="Password"
-                  name="password"
-                  className="border-2 rounded-md p-3 mb-2 w-full outline-indigo-400"
-                />
+                <div className="relative">
+                  <LoginInput
+                    type={showPassword ? 'text' : 'password'}
+                    placeholder="Password"
+                    name="password"
+                    className="border-2 rounded-md p-3 mb-2 w-full"
+                    onChange={loginHandler}
+                  />
+                  <img
+                    onClick={togglePassword}
+                    src={showPassword ? eyeShow : eyeHide}
+                    alt="eye icon"
+                    className="w-5 absolute top-4 right-5 cursor-pointer"
+                  />
+                </div>
                 <LoginButton
                   name="Log In"
                   type="submit"
