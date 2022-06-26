ee04b3a748ff08b268067f259526f3003ff47882
AbdelHameed-Sayed <94986615+AbdelHameed-Sayed@users.noreply.github.com>
2022-06-26 14:02:40 +0200

    message

---
+++ FE/src/components/login/LoginForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import facebook from '../../assets/icons/facebook.svg';
+
+const LoginForm = () => {
+  return (
+    <div>
+      <div>
+        <img src={facebook} alt="" />
+        <p>
+          Facebook helps you connect and share with the people in your life.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
