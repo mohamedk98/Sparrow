f9f7c68d3b0afa93c3043c21c895c6f8190a8af1
unknown <rana_ahmed_k@hotmail.com>
2022-06-28 00:17:31 +0200

    test

--- FE/src/components/login/footer.js
+++ FE/src/components/login/footer.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Footer = ( ) => {
     return (
-        <div className='bg-slate-100 my-16 text-xl grid lg:grid-cols-3 items-center'>
+        <div className='bg-white my-16 text-xl grid lg:grid-cols-3 items-center'>
             <div className='py-4 text-center'>
                 <p className='p-4 underline underline-offset-2'>Contact Us:</p>
-                <p className='pl-4 text-lg'><a href=''>Mohamed Khaled</a></p>
-                <p className='pl-4 text-lg'><a href=''>AbdelHameed Sayed</a></p>
+                <p className='pl-4 text-lg'><a href='https://www.linkedin.com/in/mohamedk98/' target={"_blank"}>Mohamed Khaled</a></p>
+                <p className='pl-4 text-lg'><a href='https://www.linkedin.com/in/abdelhameed-sayed-490545202' target={"_blank"}>AbdelHameed Sayed</a></p>
                 <p className='pl-4 text-lg'><a href='https://www.linkedin.com/in/rana-ahmed-k/' target={"_blank"}>Rana Ahmed</a></p>
-                <p className='pl-4 text-lg'><a href=''>Ali Maher</a></p>
-                <p className='pl-4 text-lg'><a href=''>Sara Ahmed</a></p>
+                <p className='pl-4 text-lg'><a href='https://www.linkedin.com/in/ali-maher-84199823b' target={"_blank"}>Ali Maher</a></p>
+                <p className='pl-4 text-lg'><a href='https://www.linkedin.com/in/sarah-ahmed-s/' target={"_blank"}>Sara Ahmed</a></p>
             </div>
             <div  className=''>
                 <div className='flex justify-around'>
--- FE/src/pages/login/Login.js
+++ FE/src/pages/login/Login.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
 import LoginForm from '../../components/login/LoginForm';
+import Footer from '../../components/login/Footer';
 
 const Login = () => {
   return (
     <div className="bg-gray-200 h-full pt-24">
       <LoginForm />
+      <Footer />
     </div>
   );
 };
