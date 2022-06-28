78340452c98aadb485987940081fecac1c18bb7e
AbdelHameed-Sayed <94986615+AbdelHameed-Sayed@users.noreply.github.com>
2022-06-28 23:28:02 +0200

    Refactor login form, signUp form, and add the reset form

---
+++ FE/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js}',
+    './node_modules/tw-elements/dist/js/**/*.js',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        facebook: {
+          blue: '#1877f2',
+          blueHover: '#1561c5',
+          green: '#42b72a',
+          greenHover: '#349421db',
+        },
+      },
+    },
+  },
+  plugins: [require('tw-elements/dist/plugin')],
+};
