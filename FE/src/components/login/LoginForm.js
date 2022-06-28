7925f0d0ad71f0b77dd38758e4715c61afc99b60
AbdelHameed-Sayed <94986615+AbdelHameed-Sayed@users.noreply.github.com>
2022-06-28 23:14:49 +0200

    Refactor login form, signUp form, and add the reset form

--- FE/src/components/login/LoginForm.js
+++ FE/src/components/login/LoginForm.js
@@ -1,59 +1,10 @@
-import { Formik, Form } from 'formik';
-import * as Yup from 'yup';
-import { Link, useNavigate } from 'react-router-dom';
-import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+import React from 'react';
 import facebook from '../../assets/icons/facebook.svg';
-import eyeShow from '../../assets/icons/eye-password-show.svg';
-import eyeHide from '../../assets/icons/eye-password-hide.svg';
-import LoginInput from './LoginInput';
 import LoginButton from './LoginButton';
-import axiosInstance from '../../network/axiosInstance';
-
-// User intial info:
-const loginInfo = {
-  email: '',
-  password: '',
-  hasExpiry: false,
-};
+import Formic from './Formic';
 
 const LoginForm = () => {
-  // Spineer:
-  const [showSinner, setShowSpinner] = useState(false);
-
-  // To redirect to home page after submitting form:
-  let navigate = useNavigate();
-
-  // To show form submition error if exists:
-  const [formError, setFormError] = useState('');
-
-  // For password Eye hide/show:
-  const [showPassword, setShowPassword] = useState(false);
-
-  const togglePassword = () => {
-    setShowPassword(!showPassword);
-  };
-
-  // To handle rember me:
-  const [hasExpiry, setHasExpiry] = useState(false);
-
-  // For form validation:
-  const [login, setLogin] = useState(loginInfo);
-  const { email, password } = login;
-  // console.log(email, password);
-
-  // To get input data:
-  const loginHandler = e => {
-    // console.log(e.target.name, e.target.value);
-    const { name, value } = e.target;
-    setLogin({ ...login, [name]: value });
-  };
-
-  // Validation schema:
-  const loginValidation = Yup.object({
-    email: Yup.string().required('Email address is required').email().max(100),
-    password: Yup.string().required('Password is required'),
-  });
-
   return (
     <div className="grid lg:grid-cols-2 grid-cols-1">
       <div className="mt-24 text-2xl text-center lg:text-left px-12 flex flex-col mx-auto">
@@ -68,99 +19,9 @@ const LoginForm = () => {
       </div>
       <div className="lg:ml-24 mx-auto mt-10 w-3/4 text-center">
         <div className="block rounded-lg shadow-lg bg-white p-4">
-          <Formik
-            enableReinitialize // To inforce it to teset form input values when initialValues changes.
-            initialValues={{ email, password, hasExpiry }}
-            validationSchema={loginValidation}
-            onSubmit={values => {
-              // console.log(values);
-
-              setFormError('');
-
-              setShowSpinner(!showSinner);
-
-              axiosInstance
-                .post('/login', {
-                  email: values.email,
-                  password: values.password,
-                })
-                .then(response => {
-                  console.log(response);
-                  if (response.data) setShowSpinner(showSinner);
-                  navigate('/');
-                })
-                .catch(error => {
-                  // console.log(error.response.data.message);
-                  setFormError(error.response.data.message || 'Network Error');
-                  if (error.response) setShowSpinner(showSinner);
-                });
-            }}
-          >
-            {formic => (
-              <Form onSubmit={formic.handleSubmit} className="flex flex-col">
-                <LoginInput
-                  name="email"
-                  type="text"
-                  placeholder="Email address"
-                  className="border-2 rounded-md p-3 mb-3 w-full"
-                  onChange={loginHandler}
-                />
-                <div className="relative">
-                  <LoginInput
-                    type={showPassword ? 'text' : 'password'}
-                    placeholder="Password"
-                    name="password"
-                    className="border-2 rounded-md p-3 mb-2 w-full"
-                    onChange={loginHandler}
-                  />
-                  {formic.getFieldMeta('password').value && (
-                    <img
-                      onClick={togglePassword}
-                      src={showPassword ? eyeShow : eyeHide}
-                      alt="eye icon"
-                      className="w-5 absolute top-4 right-5 cursor-pointer"
-                    />
-                  )}
-                </div>
-
-                <div>
-                  <input
-                    type="checkbox"
-                    id="hasExpiry"
-                    name="hasExpiry"
-                    onClick={() => {
-                      setHasExpiry(!hasExpiry);
-                    }}
-                    onChange={loginHandler}
-                    className="mr-2"
-                  />
-                  <label htmlFor="hasExpiry">Remember me</label>
-                </div>
-
-                {formError && (
-                  <div className="text-red-500 text-center font-bold bg-red-200 py-2 shadow-slate-400 shadow-md mt-3 mb-2">
-                    {formError}
-                  </div>
-                )}
-                <LoginButton
-                  name={
-                    showSinner ? (
-                      <div
-                        className="spinner-border animate-spin w-8 h-8 border-4 rounded-full text-cyan-200 mx-auto"
-                        role="status"
-                      ></div>
-                    ) : (
-                      'Log In'
-                    )
-                  }
-                  type="submit"
-                  className="bg-facebook-blue text-white font-bold text-lg border-2 rounded-md border-facebook-blue hover:bg-facebook-blueHover py-2 mt-3"
-                />
-              </Form>
-            )}
-          </Formik>
+          <Formic />
           <div className="text-blue-500 mt-4 mb-5 hover:underline">
-            <Link to="/forget">Forgot password?</Link>
+            <Link to="/reset">Forgot password?</Link>
           </div>
           <div className="py-3 px-6 border-t border-gray-300 text-gray-600">
             <LoginButton
--- FE/src/components/login/footer.js
+++ FE/src/components/login/footer.js
@@ -1,29 +1,44 @@
 import React from 'react';
 
-const Footer = ( ) => {
-    return (
-        <div className='bg-slate-100 my-16 text-xl grid lg:grid-cols-3 items-center'>
-            <div className='py-4 text-center'>
-                <p className='p-4 underline underline-offset-2'>Contact Us:</p>
-                <p className='pl-4 text-lg'><a href=''>Mohamed Khaled</a></p>
-                <p className='pl-4 text-lg'><a href=''>AbdelHameed Sayed</a></p>
-                <p className='pl-4 text-lg'><a href='https://www.linkedin.com/in/rana-ahmed-k/' target={"_blank"}>Rana Ahmed</a></p>
-                <p className='pl-4 text-lg'><a href=''>Ali Maher</a></p>
-                <p className='pl-4 text-lg'><a href=''>Sara Ahmed</a></p>
-            </div>
-            <div  className=''>
-                <div className='flex justify-around'>
-                    <p className='hover:cursor-pointer m-4'>English</p>
-                    <p className='hover:cursor-pointer m-4'>عربي</p>
-                </div>
-            </div>
-            <div className=''>
-                <div className='my-4'>
-                    <p className='text-center'>Made by <span className='text-red-500'>&hearts;</span> RASMA Team &copy; 2022</p>
-                </div>
-            </div>
+const Footer = () => {
+  return (
+    <div className="bg-slate-100 my-16 text-xl grid lg:grid-cols-3 items-center">
+      <div className="py-4 text-center">
+        <p className="p-4 underline underline-offset-2">Contact Us:</p>
+        <p className="pl-4 text-lg">
+          <a href="">Mohamed Khaled</a>
+        </p>
+        <p className="pl-4 text-lg">
+          <a href="">AbdelHameed Sayed</a>
+        </p>
+        <p className="pl-4 text-lg">
+          <a href="https://www.linkedin.com/in/rana-ahmed-k/" target={'_blank'}>
+            Rana Ahmed
+          </a>
+        </p>
+        <p className="pl-4 text-lg">
+          <a href="">Ali Maher</a>
+        </p>
+        <p className="pl-4 text-lg">
+          <a href="">Sara Ahmed</a>
+        </p>
+      </div>
+      <div className="">
+        <div className="flex justify-around">
+          <p className="hover:cursor-pointer m-4">English</p>
+          <p className="hover:cursor-pointer m-4">عربي</p>
         </div>
-    );
+      </div>
+      <div className="">
+        <div className="my-4">
+          <p className="text-center">
+            Made by <span className="text-red-500">&hearts;</span> RASMA Team
+            &copy; 2022
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
