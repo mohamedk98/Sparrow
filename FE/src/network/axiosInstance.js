101a7d9acf62a442c9b2bbc595ec047f75c992c0
AbdelHameed-Sayed <94986615+AbdelHameed-Sayed@users.noreply.github.com>
2022-06-29 23:38:26 +0200

    Delete FE directory

--- FE/src/network/axiosInstance.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-  baseURL: 'https://zombie-hat.herokuapp.com',
-  withCredentials: true, // to send token to backend for authorization reasons.
-});
-
-export default axiosInstance;
