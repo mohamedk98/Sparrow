6d5e3e3fb3e69b2d3e1c27d2b456e1634ddd43ef
unknown <rana_ahmed_k@hotmail.com>
2022-07-03 21:33:23 +0200

    profile

--- FE/src/App.js
+++ FE/src/App.js
@@ -8,6 +8,7 @@ import Error from "./pages/error/Error";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Reset from "./pages/reset/Reset";
+import Profile from "./pages/profile/Profile";
 import {
   addAuthentication,
   removeAuthentication,
@@ -45,6 +46,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/reset" element={<Reset />} />
+      <Route path="/profile" element={<Profile />} />
       <Route path="*" element={<Error />} />
     </Routes>
   );
