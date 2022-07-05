bcd033dc12f49405c621f1d5591d78cfe542b64c
AbdelHameed-Sayed <94986615+AbdelHameed-Sayed@users.noreply.github.com>
2022-07-05 21:56:44 +0200

    Date fixed

--- FE/src/network/axiosInstance.js
+++ FE/src/network/axiosInstance.js
@@ -1,18 +1,18 @@
-import axios from "axios";
-import { addAuthentication } from "../store/userSlice/UserSlice";
+import axios from 'axios';
+import { addAuthentication } from '../store/userSlice/UserSlice';
 //Note: using store functions to manipulate the user store as
 //we cannot use useDispatch or useSelector since this file is not a function
-import { store } from "../store/store";
+import { store } from '../store/store';
 
 //Normal instance for any request that doesn't need refresh token or authentication
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000", //"https://zombie-hat.herokuapp.com"
+  baseURL: 'https://zombie-hat.herokuapp.com', //"https://zombie-hat.herokuapp.com"
   withCredentials: true,
 });
 
 //Enhanced axios instance with interceptors to work with token refreshment
 const axiosTokenInstance = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: 'http://localhost:4000',
   withCredentials: true,
 });
 
@@ -39,10 +39,10 @@ axiosTokenInstance.interceptors.request.use(
 //Resposne interceptor to handle token refreshment
 axiosTokenInstance.interceptors.response.use(
   //if reponse is fine just return it
-  (response) => {
+  response => {
     return response;
   },
-  async (error) => {
+  async error => {
     //if there is 401 or 403 error, just refresh the token
     //original request configuration
     const originalConfig = error.config;
@@ -52,10 +52,10 @@ axiosTokenInstance.interceptors.response.use(
       try {
         //using normal instance to not fall in 401 infintie loop
         const newRefreshToken = await axiosInstance.post(
-          "/token",
+          '/token',
           store?.getState()?.user.authenticationData
         );
-            console.log(newRefreshToken.data)
+        console.log(newRefreshToken.data);
         store.dispatch(addAuthentication(newRefreshToken.data));
         return axiosTokenInstance(originalConfig);
       } catch (_error) {
