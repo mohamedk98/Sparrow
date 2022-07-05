ee5ead36b34ceaa8766403516ed1584f24d7340a
mohamedk98 <mohamedk98498@gmail.com>
2022-07-05 23:35:29 +0200

    Update LeftSideBar.js
    added logout functionality

--- FE/src/components/home/LeftSideBar/LeftSideBar.js
+++ FE/src/components/home/LeftSideBar/LeftSideBar.js
@@ -1,35 +1,49 @@
-import React, { useState } from 'react';
-import { BsFillArrowLeftCircleFill } from 'react-icons/bs';
-import { FaUserAlt } from 'react-icons/fa';
-import { FaUserFriends } from 'react-icons/fa';
-import { MdGroups } from 'react-icons/md';
-import { BsFillCalendarEventFill } from 'react-icons/bs';
-import { BsFillAlarmFill } from 'react-icons/bs';
-import { BsStarFill } from 'react-icons/bs';
-import profileImg from '../../../assets/images/default_profile.png';
+import React, { useState } from "react";
+import { BsFillArrowLeftCircleFill } from "react-icons/bs";
+import { FaUserAlt } from "react-icons/fa";
+import { FaUserFriends } from "react-icons/fa";
+import { MdGroups } from "react-icons/md";
+import { BsFillCalendarEventFill } from "react-icons/bs";
+import { BsFillAlarmFill } from "react-icons/bs";
+import { BsStarFill } from "react-icons/bs";
+import { BiLogOut } from "react-icons/bi";
+import profileImg from "../../../assets/images/default_profile.png";
+import { axiosInstance } from "../../../network/axiosInstance";
+import { useDispatch } from "react-redux";
+import { removeAuthentication } from "../../../store/userSlice/UserSlice";
+import { useNavigate } from "react-router-dom";
 
 const LeftSideBar = () => {
   const [open, setOpen] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const Menus = [
-    { id: 1, title: 'Friends', icon: { iconTitle: FaUserAlt } },
-    { id: 2, title: 'Groups', icon: { iconTitle: FaUserFriends } },
-    { id: 3, title: 'Saved', icon: { iconTitle: MdGroups } },
-    { id: 4, title: 'Events', icon: { iconTitle: BsFillCalendarEventFill } },
-    { id: 5, title: 'Most recent', icon: { iconTitle: BsFillAlarmFill } },
-    { id: 6, title: 'Favourites', icon: { iconTitle: BsStarFill } },
+    { id: 1, title: "Friends", icon: { iconTitle: FaUserAlt } },
+    { id: 2, title: "Groups", icon: { iconTitle: FaUserFriends } },
+    { id: 3, title: "Saved", icon: { iconTitle: MdGroups } },
+    { id: 4, title: "Events", icon: { iconTitle: BsFillCalendarEventFill } },
+    { id: 5, title: "Most recent", icon: { iconTitle: BsFillAlarmFill } },
+    { id: 6, title: "Favourites", icon: { iconTitle: BsStarFill } },
   ];
 
+  //logout functionality
+  const logoutHandler = () => {
+    axiosInstance.post("/logout").then(() => {
+      dispatch(removeAuthentication());
+      navigate("/login");
+    });
+  };
   return (
     <div className="flex">
       <div
         className={`bg-facebook-grey h-screen p-5 pt-8 ${
-          open ? 'w-72' : 'w-20'
+          open ? "w-72" : "w-20"
         } duration-300  relative`}
       >
         <BsFillArrowLeftCircleFill
           onClick={() => setOpen(!open)}
           className={`bg-facebook-grey text-facebook-blue  text-3xl rounded-full cursor-pointer ${
-            !open && 'rotate-180'
+            !open && "rotate-180"
           } absolute -right-3 top-9 border border-facebook-grey`}
         />
         <ul>
@@ -37,7 +51,7 @@ const LeftSideBar = () => {
             <img src={profileImg} alt="profile" className="rounded-full" />
             <span
               className={`text-black origin-left font-bold text-sm mt-3 ml-2 duration-300 ${
-                !open && 'scale-0 -mb-7'
+                !open && "scale-0 -mb-7"
               }`}
             >
               User Name
@@ -46,7 +60,7 @@ const LeftSideBar = () => {
         </ul>
 
         <ul className="pt-2">
-          {Menus.map(menu => (
+          {Menus.map((menu) => (
             <li
               key={menu.id}
               className=" text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-facebook-greyHover rounded-md mt-2"
@@ -56,13 +70,29 @@ const LeftSideBar = () => {
               </span>
               <span
                 className={`text-base font-medium text-black flex-1 ${
-                  !open && 'hidden'
+                  !open && "hidden"
                 }`}
               >
                 {menu.title}
               </span>
             </li>
           ))}
+          {/* Logout menu item */}
+          <li
+            className=" text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-facebook-greyHover rounded-md mt-2"
+            onClick={logoutHandler}
+          >
+            <span className="text-2xl block float-left text-facebook-blue">
+              <BiLogOut />
+            </span>
+            <span
+              className={`text-base font-medium text-black flex-1 ${
+                !open && "hidden"
+              }`}
+            >
+              Logout
+            </span>
+          </li>
         </ul>
       </div>
     </div>
