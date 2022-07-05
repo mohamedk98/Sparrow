3ed978943e3dd7e751656c4872b25fef04fa7ee5
mohamedk98 <mohamedk98498@gmail.com>
2022-07-05 17:35:50 +0200

    Update authenticationController.js

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -19,47 +19,48 @@ const signup = (req, res, next) => {
   const password = req.body.password;
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
-  const day = req.body.day;
-  const month = req.body.month;
-  const year = req.body.year;
+  // const day = req.body.day;
+  // const month = req.body.month;
+  // const year = req.body.year;
   const gender = req.body.gender;
+  const date = req.body.date;
   const dateOfBirth = new Date(`${day}/${month}/${year}`)
     .toLocaleString()
     .split(",")[0];
   //if the email or username was already used,don't create account
   //else,create a new account
 
-  try {
-    User.findOne({ email }).then((user) => {
-      if (!user) {
-        bcrypt.hash(password, 12).then((hashedPassword) => {
-          const user = new User({
-            //generate Random userID
-            userId: crypto.randomBytes(16).toString("hex"),
-            //generate unique username by adding firstName-lastName-random number
-            username: `${firstName}-${lastName}-${crypto
-              .randomBytes(12)
-              .toString("hex")}`,
-            email: email,
-            password: hashedPassword,
-            firstName: firstName,
-            lastName: lastName,
-            dateOfBirth: dateOfBirth,
-            age: new Date().getFullYear() - new Date(dateOfBirth).getFullYear(),
-            gender: gender,
-          });
-          user.save();
-          res.status(200).send({ message: "Successfully registered" });
+  User.findOne({ email }).then((user) => {
+    if (!user) {
+      bcrypt.hash(password, 12).then((hashedPassword) => {
+        const user = new User({
+          //generate Random userID
+          userId: crypto.randomBytes(16).toString("hex"),
+          //generate unique username by adding firstName-lastName-random number
+          username: `${firstName}-${lastName}-${crypto
+            .randomBytes(12)
+            .toString("hex")}`,
+          email: email,
+          password: hashedPassword,
+          firstName: firstName,
+          lastName: lastName,
+          dateOfBirth: date,
+          age: new Date().getFullYear() - new Date(dateOfBirth).getFullYear(),
+          gender: gender,
         });
-      } else {
-        res.status(400).send({ message: "User Already Registered" });
-      }
-    });
-  } catch {
-    res
-      .send(400)
-      .send({ message: "An error has occured , please try again later" });
-  }
+        try {
+          user.save();
+        } catch {
+          res
+            .send(400)
+            .send({ message: "An error has occured , please try again later" });
+        }
+        res.status(200).send({ message: "Successfully registered" });
+      });
+    } else {
+      res.status(400).send({ message: "User Already Registered" });
+    }
+  });
 };
 
 /**Login Controller */
@@ -108,7 +109,7 @@ const login = async (req, res, next) => {
             .cookie("access_token", accessToken, {
               httpOnly: true,
               secure: true,
-              sameSite:"none",
+              sameSite: "none",
               //1 day token
               expires: hasExpiry
                 ? new Date(Date.now() + 24 * 60 * 60 * 1000)
@@ -178,7 +179,7 @@ const autoLogin = async (req, res) => {
     .cookie("access_token", accessToken, {
       httpOnly: true,
       secure: true,
-      sameSite:"none",
+      sameSite: "none",
       //1 day token
       expires: accessTokenData.hasExpiry
         ? new Date(Date.now() + 24 * 60 * 60 * 1000)
