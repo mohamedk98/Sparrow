d9afcad4ee7e62560b10ad9cac4ec5effee28a7a
AbdelHameed-Sayed <94986615+AbdelHameed-Sayed@users.noreply.github.com>
2022-07-15 22:17:56 +0200

    Post added

--- FE/src/pages/home/Home.js
+++ FE/src/pages/home/Home.js
@@ -20,7 +20,7 @@ const Home = () => {
   //     });
   // }, [navigate]);
   return (
-    <div className="h-screen bg-facebook-grey overflow-hidden">
+    <div className="h-screen bg-facebook-grey">
       <Header />
       <div className="flex">
         <LeftSideBar />
