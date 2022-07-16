03fe97dadef7a7bc1a4c60d65a52dc3cbc87f69b
AbdelHameed-Sayed <94986615+AbdelHameed-Sayed@users.noreply.github.com>
2022-07-16 23:50:38 +0200

    Update Feed.js

--- FE/src/components/home/Feed/Feed.js
+++ FE/src/components/home/Feed/Feed.js
@@ -1,22 +1,16 @@
-import React from 'react'
-import InputBox from './InputBox'
-import Post from './posts/Post'
+import React from 'react';
+import InputBox from './InputBox';
+import Post from './posts/Post';
 
 const Feed = () => {
-  
   return (
-    <div className='flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto '>
-        <div className='mx-auto max-w-md md:max-w-lg lg:max-w-2xl'>
-          <InputBox/>
-          <Post/>
-        </div>
-
-      
-        </div>
-       
-
-    
-  )
-}
+    <div className="flex-grow h-screen pb-44 pt-6 mx-auto ">
+      <div className="mx-auto max-w-md md:max-w-lg lg:max-w-2xl">
+        <InputBox />
+        <Post />
+      </div>
+    </div>
+  );
+};
 
 export default Feed;
