f5a4192fe5e7a168b3dd68a0d0d81c2b39d3de74
sarah ahmed <sarahahmed7088@gmail.com>
2022-07-21 12:20:47 +0200

    create post

---
+++ src/components/home/Feed/PostBtn.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PostBtn = () => {
+  return (
+    <button
+    type="submit"
+    className="inline-block px-6 w-full py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out ml-1">
+    Post
+  </button>
+  )
+}
+
+export default PostBtn
