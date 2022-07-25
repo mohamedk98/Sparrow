5e8fa53180b7f6abb3262c2803debc9e4f90693b
mohamedk98 <mohamedk98498@gmail.com>
2022-07-25 11:26:33 +0200

    Update userApi.js

--- datasources/userApi.js
+++ datasources/userApi.js
@@ -81,7 +81,7 @@ class UserApi {
     try {
       let newProfile =await userData.save();
       return {
-         newProfile
+         newProfile,
         httpStatusCode: 200,
       };
     } catch {
