4e4ee1e2e386f51abb4d246ee616059bee5739ac
mohamedk98 <mohamedk98498@gmail.com>
2022-07-31 10:53:27 +0200

    Update Posts.js

--- models/Posts.js
+++ models/Posts.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const postSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
+  userId: { type: mongoose.Types.ObjectId, ref:"User",required: true },
   creatorName: { type: String, required: true },
   createdAt: { type: String, required: true },
   content: { type: String, required: true },
