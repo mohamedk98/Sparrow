154e67f9862c3004eb3f1dd3da354fc222284ca0
Rana-a-k <rana_ahmed_k@hotmail.com>
2022-08-15 13:42:48 +0200

    admin auth

--- routes/adminAuthentication.js
+++ routes/adminAuthentication.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {adminLogin} = require("../controllers/adminAuthenticationController");
+const {adminLogin, adminLogout} = require("../controllers/adminAuthenticationController");
 
 //Login handler
 router.post("/admin/login", adminLogin)
+
+//Logout handler
+router.get("/admin/logout", adminLogout)
 
 module.exports = router;
