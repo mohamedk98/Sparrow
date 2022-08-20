872f40bae3dfc60cf12e366245e5aee780a11054
mohamedk98 <mohamedk98498@gmail.com>
2022-08-20 13:46:56 +0200

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -2,19 +2,20 @@ const express = require("express");
 const router = express.Router();
 const {getAllUsers, deleteUser, getAllPosts, deletePost, createAdmin} = require("../controllers/adminController")
 const {changeAdminImage,getAdminData} = require("../controllers/adminAuthenticationController")
-const authorization = require("../middlwares/adminAuthorization");
+const {authorization} = require("../middlwares/adminAuthorization");
 const { adminImageUpload } = require("../middlwares/fileUpload");
 
 // authorization middleware
-router.use(authorization, (req,res,next)=>{
-    if(req.role == "admin")
-    next()
-    else{
-        let error = new Error("Not Authorized")
-        error.status = 403;
-        next(error)
-    }
-})
+// router.use(authorization, (req,res,next)=>{
+//     if(req.role == "admin")
+//     next()
+//     else{
+//         let error = new Error("Not Authorized")
+//         error.status = 403;
+//         next(error)
+//     }
+// })
+router.use(authorization)
 /* Admin data router */
 router.post("/create-admin",createAdmin)
 router.post("/change-profile",adminImageUpload.single("media"),changeAdminImage)
